2bac96ce4d40442ac68ab5621372e3797cf95917
stonebridge <stonebridge@njfu.edu.cn>
2022-01-01 23:41:12 +0800

    商品分类的树形结构、展开、属性expand-on-click-node、default-expanded-keys以及通过node-key赋值

--- src/views/modules/product/category.vue
+++ src/views/modules/product/category.vue
@@ -1,6 +1,31 @@
 <template>
   <div>
-    <el-tree :data="menus" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
+    <el-tree :data="menus"
+             :props="defaultProps"
+             :show-checkbox="true"
+             :expand-on-click-node="true"
+             node-key="catId"
+             :default-expanded-keys="expandeKey"
+             @node-click="handleNodeClick">
+      <span class="custom-tree-node" slot-scope="{ node, data }">
+        <span>{{ node.label }}</span>
+        <span>
+          <el-button v-if="node.level<=2"
+                     type="text"
+                     size="mini"
+                     @click="() => append(data)">
+            Append
+          </el-button>
+          <el-button
+            v-if="node.childNodes.length===0"
+            type="text"
+            size="mini"
+            @click="() => remove(node, data)">
+            Delete
+          </el-button>
+        </span>
+      </span>
+    </el-tree>
   </div>
 </template>
 <script>
@@ -9,6 +34,7 @@ export default {
   data () {
     return {
       menus: [],
+      expandeKey: [],
       defaultProps: {
         children: 'children',
         label: 'name'
@@ -26,9 +52,40 @@ export default {
       }).then(({data}) => {
         this.menus = data.data
       })
+    },
+    append (data) {
+      console.log('append' + data)
+    },
+    remove (node, data) {
+      var ids = [data.catId]
+      this.$confirm(`是否删除【${data.name}】, 是否继续?`, '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        this.$http({
+          url: this.$http.adornUrl('/mallproduct/category/delete'),
+          method: 'post',
+          data: this.$http.adornData(ids, false)
+        }).then(({data}) => {
+          this.getMenus()
+          console.log(node)
+          console.log(node.parent.data.catId)
+          this.expandeKey = [node.parent.data.catId]
+          this.$message({
+            type: 'success',
+            message: '删除成功!'
+          })
+        })
+      }).catch(() => {
+        this.$message({
+          type: 'info',
+          message: '已取消删除'
+        })
+      })
     }
   },
-  mounted () {
+  created () {
     this.getMenus()
   }
 }
