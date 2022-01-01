c9824e66da0d6f6435400a7eaf926c61aec0c171
stonebridge <stonebridge@njfu.edu.cn>
2022-01-01 19:59:18 +0800

    first commit

---
+++ src/views/modules/product/category.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <el-tree :data="menus" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
+  </div>
+</template>
+<script>
+export default {
+  name: 'category.vue',
+  data () {
+    return {
+      menus: [],
+      defaultProps: {
+        children: 'children',
+        label: 'name'
+      }
+    }
+  },
+  methods: {
+    handleNodeClick (data) {
+      console.log(data)
+    },
+    getMenus () {
+      this.$http({
+        url: this.$http.adornUrl('/mallproduct/category/list/tree'),
+        method: 'get'
+      }).then(({data}) => {
+        this.menus = data.data
+      })
+    }
+  },
+  mounted () {
+    this.getMenus()
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
