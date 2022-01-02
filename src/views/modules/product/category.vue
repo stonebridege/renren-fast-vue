b419a1ba57f3554268413a1d265e03b8700fa6d5
stonebridge <stonebridge@njfu.edu.cn>
2022-01-02 19:56:19 +0800

    1.属性expand-on-click-node：是否在点击节点的时候展开或者收缩节点， 默认值为 true。应该设置为false，因为在新增节点或者删除的时候点击按钮会触发节点的收缩或者展开； 2.完成新增节点功能，保存后刷新树并展开原来的节点

--- src/views/modules/product/category.vue
+++ src/views/modules/product/category.vue
@@ -3,7 +3,7 @@
     <el-tree :data="menus"
              :props="defaultProps"
              :show-checkbox="true"
-             :expand-on-click-node="true"
+             :expand-on-click-node="false"
              node-key="catId"
              :default-expanded-keys="expandeKey"
              @node-click="handleNodeClick">
@@ -26,6 +26,17 @@
         </span>
       </span>
     </el-tree>
+    <el-dialog title="新增节点" :visible.sync="dialogVisible" width="30%">
+      <el-form :model="category">
+        <el-form-item label="分类名称">
+          <el-input v-model="category.name" autocomplete="off"></el-input>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+    <el-button @click="dialogVisible = false">取 消</el-button>
+    <el-button type="primary" @click="addCategory">确 定</el-button>
+  </span>
+    </el-dialog>
   </div>
 </template>
 <script>
@@ -33,6 +44,8 @@ export default {
   name: 'category.vue',
   data () {
     return {
+      category: {name: '', parentCid: 0, catLevel: 0, showStatus: 1, sort: 0},
+      dialogVisible: false,
       menus: [],
       expandeKey: [],
       defaultProps: {
@@ -43,7 +56,29 @@ export default {
   },
   methods: {
     handleNodeClick (data) {
-      console.log(data)
+      // console.log(data)
+    },
+    append (data) {
+      this.category.parentCid = data.catId
+      this.category.catLevel = data.catLevel * 1 + 1
+      this.dialogVisible = true
+    },
+    // 添加分类
+    addCategory () {
+      this.dialogVisible = false
+      this.$http({
+        url: this.$http.adornUrl('/mallproduct/category/save'),
+        method: 'post',
+        data: this.$http.adornData(this.category, false)
+      }).then(() => {
+        this.getMenus()
+        this.expandeKey = [this.category.parentCid]
+        this.category.name = ''
+        this.$message({
+          type: 'success',
+          message: '保存成功!'
+        })
+      })
     },
     getMenus () {
       this.$http({
@@ -52,9 +87,6 @@ export default {
       }).then(({data}) => {
         this.menus = data.data
       })
-    },
-    append (data) {
-      console.log('append' + data)
     },
     remove (node, data) {
       var ids = [data.catId]
