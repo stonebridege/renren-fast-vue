d44c01beb1392ae244a66ca7f198ea4d6d4042a5
stonebridge <stonebridge@njfu.edu.cn>
2022-01-08 17:28:15 +0800

    商品分类拖拽修改

--- src/views/modules/product/category.vue
+++ src/views/modules/product/category.vue
@@ -2,6 +2,7 @@
   <div>
     <el-switch v-model="draggable" active-text="开启拖拽" inactive-text="关闭拖拽"></el-switch>
     <el-button @click="batchSave">批量保存</el-button>
+    <el-button type="danger" @click="batchDelete">批量删除</el-button>
     <el-tree :data="menus"
              :props="defaultProps"
              :show-checkbox="true"
@@ -10,7 +11,8 @@
              :draggable="draggable"
              :allow-drop="allowDrop"
              @node-drop="handleDrop"
-             :default-expanded-keys="expandeKey">
+             :default-expanded-keys="expandeKey"
+             ref="menuTree">
       <span class="custom-tree-node" slot-scope="{ node, data }">
         <span>{{ node.label }}</span>
         <span>
@@ -80,6 +82,34 @@ export default {
         this.maxLevel = 0
         // this.pCid = 0;
       })
+    },
+    batchDelete () {
+      let catIds = []
+      let checkedNodes = this.$refs.menuTree.getCheckedNodes()
+      console.log('被选中的元素', checkedNodes)
+      for (let i = 0; i < checkedNodes.length; i++) {
+        catIds.push(checkedNodes[i].catId)
+      }
+      this.$confirm(`是否批量删除【${catIds}】菜单?`, '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      })
+        .then(() => {
+          this.$http({
+            url: this.$http.adornUrl('/mallproduct/category/delete'),
+            method: 'post',
+            data: this.$http.adornData(catIds, false)
+          }).then(({data}) => {
+            this.$message({
+              message: '菜单批量删除成功',
+              type: 'success'
+            })
+            this.getMenus()
+          })
+        })
+        .catch(() => {
+        })
     },
     handleDrop (draggingNode, dropNode, dropType, ev) {
       console.log('handleDrop: ', draggingNode, dropNode, dropType)
