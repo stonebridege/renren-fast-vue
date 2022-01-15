d052ad44b261f1a69ba8209002ff904006ceb5a6
stonebridge <stonebridge@njfu.edu.cn>
2022-01-15 17:50:28 +0800

    商品品牌完成校验功能

--- src/views/modules/product/brand-add-or-update.vue
+++ src/views/modules/product/brand-add-or-update.vue
@@ -80,7 +80,7 @@ export default {
             validator: (rule, value, callback) => {
               if (value === "") {
                 callback(new Error("排序字段必须填写"));
-              } else if (!Number.isInteger(value) || value<0) {
+              } else if (!Number.isInteger(parseInt(value)) || value < 0) {
                 callback(new Error("排序必须是一个大于等于0的整数"));
               } else {
                 callback();
--- src/views/modules/product/brand.vue
+++ src/views/modules/product/brand.vue
@@ -148,7 +148,7 @@ export default {
     updateBrandStatus (data) {
       let {brandId, showStatus} = data
       this.$http({
-        url: this.$http.adornUrl('/mallproduct/brand/update'),
+        url: this.$http.adornUrl('/mallproduct/brand/update/status'),
         method: 'post',
         data: this.$http.adornData({brandId, showStatus: showStatus ? 1 : 0}, false)
       }).then(({data}) => {
